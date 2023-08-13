3986f1866176693b87be91002c3786edfbbf7339
Marco <patrickstar1281@yahoo.com>
2023-08-13 16:22:51 -0500

    added an instruments class

---
+++ models/instrument.py
@@ -0,0 +1,23 @@
+class Instrument:
+
+    def __init__(self, name, ins_type, displayName, pipLocation, tradeUnitsPrecision, marginRate):
+        self.name = name
+        self.ins_type = ins_type
+        self.displayName = displayName
+        self.pipLocation = pow(10, pipLocation)
+        self.tradeUnitsPrecision = tradeUnitsPrecision
+        self.marginRate = float(marginRate)
+
+    def __repr__(self):
+        return str(vars(self))
+    
+    @classmethod
+    def fromApiObject(cls, ob):
+        return Instrument(
+            ob['name'],
+            ob['type'],
+            ob['displayName'],
+            ob['pipLocation'],
+            ob['tradeUnitsPrecision'],
+            ob['marginRate']
+        )
