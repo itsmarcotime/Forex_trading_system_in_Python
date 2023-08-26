0ae9d05fcb3d74ab0099cbe1214e026a75b26565
Marco <patrickstar1281@yahoo.com>
2023-08-26 17:41:03 -0500

    finding profit or lose in MA

--- jupyter_notes/plotting.py
+++ jupyter_notes/plotting.py
@@ -2,8 +2,9 @@ import datetime as dt
 import plotly.graph_objects as go
 
 class CandlePlot:
-    def __init__(self, df):
+    def __init__(self, df, candles=True):
         self.df_plot = df.copy()
+        self.candles = candles
         self.create_candle_fig()
 
     def add_timestr(self):
@@ -12,19 +13,20 @@ class CandlePlot:
     def create_candle_fig(self):
         self.add_timestr()
         self.fig = go.Figure()
-        self.fig.add_trace(go.Candlestick(
-            x = self.df_plot.sTime,
-            open = self.df_plot.mid_o,
-            high = self.df_plot.mid_h,
-            low = self.df_plot.mid_l,
-            close = self.df_plot.mid_c,
-            line = dict(width=1), 
-            opacity = 1,
-            increasing_fillcolor='#24A06B',
-            decreasing_fillcolor='#CC2E3C',
-            increasing_line_color='#2EC886',
-            decreasing_line_color='#FF3A4C',
-        ))
+        if self.candles == True:
+            self.fig.add_trace(go.Candlestick(
+                x = self.df_plot.sTime,
+                open = self.df_plot.mid_o,
+                high = self.df_plot.mid_h,
+                low = self.df_plot.mid_l,
+                close = self.df_plot.mid_c,
+                line = dict(width=1), 
+                opacity = 1,
+                increasing_fillcolor='#24A06B',
+                decreasing_fillcolor='#CC2E3C',
+                increasing_line_color='#2EC886',
+                decreasing_line_color='#FF3A4C',
+            ))
 
     def update_layout(self, width, height, nticks):
         self.fig.update_yaxes(
