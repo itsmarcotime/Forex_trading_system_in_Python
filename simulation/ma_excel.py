9676e0bb0886ee593f8c9b23b538451aaa7f3f0d
Marco <patrickstar1281@yahoo.com>
2023-09-13 22:14:06 -0500

    charting cumulative gains

--- simulation/ma_excel.py
+++ simulation/ma_excel.py
@@ -1,5 +1,34 @@
 import pandas as pd 
 
+WIDTHS = {
+    'L:L' : 20,
+    'B:F' : 9
+}
+
+def set_widths(pair, writer):
+    worksheet = writer.sheets[pair]
+    for k,v in WIDTHS.items():
+        worksheet.set_column(k, v)
+
+def get_line_chart(book, start_row, end_row, labels_col, data_col, title, sheetname):
+    chart = book.add_chart({'type' : 'line'})
+    chart.add_series({
+        'categories' : [sheetname, start_row, labels_col, end_row, labels_col],
+        'values' : [sheetname, start_row, data_col, end_row, data_col],
+        'line': {'color': 'blue'}
+        })
+    chart.set_title({'name' : title})
+    chart.set_legend({'none' : True})
+    return chart
+
+def add_chart(pair, cross, df, writer):
+    workbook = writer.book
+    worksheet = writer.sheets[pair]
+
+    chart = get_line_chart(workbook, 1, df.shape[0], 11, 12, f"GAIN_C for {pair} {cross}", pair)
+    chart.set_size({'x_scale' : 2.5, 'y_scale' : 2.5})
+    worksheet.insert_chart('O1', chart)
+
 def add_pair_charts(df_ma_res, df_ma_trades, writer):
     cols = ['time', 'GAIN_C']
     df_temp = df_ma_res.drop_duplicates(subset="pair")
@@ -7,6 +36,8 @@ def add_pair_charts(df_ma_res, df_ma_trades, writer):
     for _, row in df_temp.iterrows():
         dft = df_ma_trades[(df_ma_trades.cross == row.cross)&(df_ma_trades.pair == row.pair)]
         dft[cols].to_excel(writer, sheet_name=row.pair, index=False, startrow=0, startcol=11)
+        set_widths(row.pair, writer)
+        add_chart(row.pair, row.cross, dft, writer)
 
 def add_pair_sheets(df_ma_res, writer):
     for p in df_ma_res.pair.unique():
