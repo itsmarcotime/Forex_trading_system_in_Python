3acd0bc3043db5e0efb0ba0b0db30bad84da8cb8
Marco <patrickstar1281@yahoo.com>
2023-09-23 18:35:13 -0500

    transfered patterns to scripts

---
+++ technicals/patterns.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def apply_candle_props(df: pd.DataFrame):
+    df_an = df.copy()
+    direction = df_an.mid_c - df_an.mid_o
+    body_size = abs(direction)
+    direction = [1 if x >= 0 else -1 for x in direction]
+    full_range = df_an.mid_h - df_an.mid_l
+    body_perc = (body_size / full_range) * 100
+    body_lower = df_an[['mid_c', 'mid_o']].min(axis=1)
+    body_upper = df_an[['mid_c', 'mid_o']].max(axis=1)
+    body_bottom_perc = ((body_lower - df_an.mid_l) / full_range) * 100
+    body_top_perc = 100 - (((df_an.mid_h - body_upper) / full_range) * 100)
+
+    df_an['body_lower'] = body_lower
+    df_an['body_upper'] = body_upper
+    df_an['body_bottom_perc'] = body_bottom_perc
+    df_an['body_top_perc'] = body_top_perc
+    df_an['body_perc'] = body_perc
+
+    return df_an
+
+def apply_patterns(df: pd.DataFrame):
+    df_an = apply_candle_props(df)
+    return df_an
