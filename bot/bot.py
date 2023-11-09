fa3a6a27f2e3e1e916710a7a089ac40473430ab8
Marco <patrickstar1281@yahoo.com>
2023-11-08 20:22:17 -0600

    setting up error and main logging

--- bot/bot.py
+++ bot/bot.py
@@ -8,10 +8,21 @@ class Bot:
 
     def __init__(self):
         self.setup_logs()
+        self.log_to_main("Bot Started")
+        self.log_to_error("Bot Started")
 
     def setup_logs(self):
         self.logs = {}
         self.logs[Bot.ERROR_LOG] = LogWrapper(Bot.ERROR_LOG)
         self.logs[Bot.MAIN_LOG] = LogWrapper(Bot.MAIN_LOG)
+
+    def log_message(self, msg, key):
+        self.logs[key].logger.debug(msg)
+
+    def log_to_main(self, msg):
+        self.log_message(msg, Bot.MAIN_LOG)
+    
+    def log_to_error(self, msg):
+        self.log_message(msg, Bot.ERROR_LOG)
 
 
