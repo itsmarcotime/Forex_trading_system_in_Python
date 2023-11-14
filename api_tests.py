6fe7d762cd711e909b55b17b31de5db7ceb7b158
Marco <patrickstar1281@yahoo.com>
2023-11-13 20:17:51 -0600

    candle timing class

--- api_tests.py
+++ api_tests.py
@@ -1,9 +1,10 @@
 from api.oanda_api import OandaApi
 from infrastructure.instrument_collection import instrumentCollection
 import time
+from models.candle_timing import CandleTiming
 
 if __name__ == '__main__':
     api = OandaApi()
     instrumentCollection.load_instruments("./data")
-    print(api.last_complete_candle("EUR_USD", granularity="M5"))
-    
+    dd = api.last_complete_candle("EUR_USD", granularity="M5")
+    print(CandleTiming(dd))
---
+++ models/candle_timing.py
@@ -0,0 +1,9 @@
+import datetime as dt
+
+class CandleTiming:
+    def __init__(self, last_time):
+        self.last_time = last_time
+        self.is_ready = False
+
+    def __repr__(self):
+        return f"last_candle:{dt.datetime.strftime(self.last_time, '%y-%m-%d %H:%m')} is_ready:{self.is_ready}"
