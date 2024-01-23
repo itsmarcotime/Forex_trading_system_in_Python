7e681446fe1ac823c45340a9e78ca35c7803033c
Marco <patrickstar1281@yahoo.com>
2024-01-22 18:15:49 -0600

    Aquiring Locks

--- api/stream_prices.py
+++ api/stream_prices.py
@@ -19,7 +19,12 @@ class PriceStreamer(threading.Thread):
 
     def update_live_price(self, live_price: LiveApiPrice):
         try:
-        except:
+            self.price_lock.acquire()
+            self.shared_prices[live_price.instrument] = live_price
+        except Exception as error:
+            self.log.logger.error(f"Exception: {error}")
+        finally:
+            self.price_lock.release()
 
     def run(self):
 
@@ -36,3 +41,4 @@ class PriceStreamer(threading.Thread):
                 decoded_price = json.loads(price.decode('utf-8'))
                 if 'type' in decoded_price and decoded_price['type'] == 'PRICE':
                     print(LiveApiPrice(decoded_price).get_dict())
+                    self.update_live_price(LiveApiPrice(decoded_price))
