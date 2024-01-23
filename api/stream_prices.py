60fce9cc17a6b86e2e55c5ca00496299cbe4d98a
Marco <patrickstar1281@yahoo.com>
2024-01-22 18:26:58 -0600

    updating shared prices

--- api/stream_prices.py
+++ api/stream_prices.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import pandas as pd
+from timeit import default_timer as timer
 import constants.defs as defs
 from models.live_api_price import LiveApiPrice
 import threading
@@ -8,6 +10,9 @@ from infrastructure.log_wrapper import LogWrapper
 STREAM_URL = f"https://stream-fxpractice.oanda.com/v3"
 
 class PriceStreamer(threading.Thread):
+
+    LOG_FREQ = 60
+
     def __init__(self, shared_prices, price_lock: threading.Lock, price_events):
         super().__init__()
         self.pairs_list = shared_prices.keys()
@@ -26,7 +31,13 @@ class PriceStreamer(threading.Thread):
         finally:
             self.price_lock.release()
 
+    def log_data(self):
+        self.log.logger.debug("")
+        self.log.logger.debug(f"\n{pd.DataFrame.from_dict([v.get_dict() for _, v in self.shared_prices.items()])}")
+
     def run(self):
+
+        start = timer() - PriceStreamer.LOG_FREQ + 10
 
         params = dict(
             instruments = ",".join(self.pairs_list)
@@ -40,5 +51,8 @@ class PriceStreamer(threading.Thread):
             if price:
                 decoded_price = json.loads(price.decode('utf-8'))
                 if 'type' in decoded_price and decoded_price['type'] == 'PRICE':
-                    print(LiveApiPrice(decoded_price).get_dict())
                     self.update_live_price(LiveApiPrice(decoded_price))
+                    if timer() - start > PriceStreamer.LOG_FREQ:
+                        print(LiveApiPrice(decoded_price).get_dict())
+                        self.log_data()
+                        start = timer()
