d64ccca1671ba1085b762c8df18b48bf63e657aa
Marco <patrickstar1281@yahoo.com>
2024-02-01 11:13:56 -0600

    extracting data

--- scraping/dailyfx_com.py
+++ scraping/dailyfx_com.py
@@ -9,4 +9,17 @@ def dailyfx_com():
     # print(resp.status_code)
 
     soup = BeautifulSoup(resp.content, 'html.parser')
-    print(soup)
+    # print(soup)
+
+    rows = soup.select('.dfx-technicalSentimentCard')
+
+    for r in rows:
+        card = r.select_one('.dfx-technicalSentimentCard__pairAndSignal')
+        print(card.select_one('a').get_text().replace('/', '_').strip(), end=' ')
+        print(card.select_one('span').get_text(), end=' ')
+        change_values = r.select('.dfx-technicalSentimentCard__changeValue')
+        print(change_values[0].get_text())
+        print(change_values[1].get_text())
+        print(change_values[3].get_text())
+        print(change_values[4].get_text())
+        print()
